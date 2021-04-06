3e2284dcc1e97acd7bc635d67c5e0df028bf813d
Peter <dhondt.peter@gmail.com>
2021-04-06 10:55:19 +0200

    added TodoContext class

---
+++ Data/TodoContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApi.Models;
+
+namespace ToDoApi.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
